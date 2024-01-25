eadf245222996cc4f600d4d35f3916afc16b3fdf
ney1102 <phantatyen1102@gmail.com>
2024-01-25 15:52:03 +0700

    create project, update login

---
+++ Controllers/AuthenticationController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Model.DTO;
+using Core.Interfaces;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FightDocManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly ILoginManager _loginManager ;
+        public IConfiguration _configuration;
+        public AuthenticationController(IConfiguration configuration,ILoginManager loginManager)
+        {
+            _configuration = configuration;
+            _loginManager = loginManager;
+        }
+       
+        // GET: api/<AuthenticationController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<AuthenticationController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/Authentication/login
+        [HttpPost("login")]
+        public async Task<IActionResult> LogIn([FromBody] LoginDTO login)
+        {
+            try
+            {
+                if (await _loginManager.LoginAsync(login.Email, login.Password))
+                {
+                    var token = new JwtSecurityToken(
+                        expires: DateTime.UtcNow.AddMinutes(60));                    
+                    return Ok(new { token = new JwtSecurityTokenHandler().WriteToken(token) });
+                }
+                return BadRequest(new
+                {
+                    message = "Username or password incorrect"
+                });
+            }
+            catch
+            {
+                return BadRequest(new
+                {
+                    message = "Username or password incorrect"
+                });
+            }
+        }
+
+        // PUT api/<AuthenticationController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<AuthenticationController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,19 @@
-var builder = WebApplication.CreateBuilder(args);
+using Core.Interfaces;
+using Core.Managers;
+using Microsoft.EntityFrameworkCore;
+using Model.Context;
 
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddControllers();
+builder.Services.AddTransient<ILoginManager, LoginManager>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
